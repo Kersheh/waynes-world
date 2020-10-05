11380acf7984b67547ef652b57fc0d37ee253620
Matt Breckon <mattbreckon@gmail.com>
2020-10-05 01:46:56 -0400

    Remove iphone forced width for full view width

--- src/app/contentView/ContentView.tsx
+++ src/app/contentView/ContentView.tsx
@@ -10,6 +10,22 @@ const ContentView = () => {
   return (
     <div className={styles.contentView}>
       <h1>{activeView}</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse id
+        tristique arcu, sed fermentum nunc. Sed ultrices mi et turpis lacinia
+        elementum. Sed imperdiet tortor arcu. Quisque porta nibh et luctus
+        gravida. Mauris mollis, quam non sodales tincidunt, nulla magna
+        tincidunt massa, id interdum lectus nulla ac felis. Nunc venenatis dui
+        nec interdum rhoncus. Vivamus lectus diam, tincidunt a porttitor in,
+        posuere et arcu. Donec consequat justo eu porta ornare. Phasellus
+        venenatis lobortis dignissim. Mauris eget eleifend nisl. Sed pharetra
+        elit diam, eget condimentum enim tempus vel. Nam sagittis sagittis arcu,
+        vel facilisis nibh mollis eu. In egestas ante dolor, vel consectetur mi
+        tristique in. Sed et erat auctor enim venenatis ultricies. Cras vitae
+        ipsum rutrum, rhoncus eros commodo, placerat felis. Fusce neque sapien,
+        gravida ac lacinia quis, pharetra nec quam.
+      </p>
     </div>
   );
 };
